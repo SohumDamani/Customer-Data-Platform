d571c6ff2967fde3dac92dc07197d4b54dc63bb2
Sohum Damani <damanisohum@gmail.com>
2026-08-21 16:28:40 -0400

    feat: add optional multi-column search to /customers, combined with pagination

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from models import Customer,db
 from sqlalchemy.orm import Session
+from sqlalchemy import or_
 from schemas import CustomerOut
+from typing import Optional
 
 api = FastAPI()
 
@@ -10,9 +12,19 @@ def get():
     return {"status": "ok"}
 
 @api.get("/customers",response_model=list[CustomerOut])
-def get_customers(page: int, page_size: int):
+def get_customers(page: int, page_size: int, search: Optional[str]=None):
     session = Session(db)
     # so page 1 
+    result = session.query(Customer)
+    if search:
+        result = result.filter(
+            or_(
+            Customer.firstname.like(f"%{search}%"),
+            Customer.lastname.like(f"%{search}%"),
+            Customer.company.like(f"%{search}%")
+            ))
     record_start = (page-1)*page_size
-    result = session.query(Customer).limit(page_size).offset(record_start).all()
+    result = result.limit(page_size).offset(record_start).all()
     return result
+
+
